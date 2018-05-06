d0d053ab7b834494993a1c3a31cb80864f38fc30
codewarrior <dobi@outlook.com>
2018-05-06 09:11:52 +0000

    new 29
    git-tfs-id: [https://pseudocode.visualstudio.com/]$/Algorithms;C59

--- Leetcode/Solutions.cpp
+++ Leetcode/Solutions.cpp
@@ -915,7 +915,7 @@ int removeElement(vector<int>& nums, int val)
 // 28. Implement strStr()
 int strStr(string text, string pattern)
 {
-    for (int i = 0, j; text.length() >= pattern.length() && i <= text.length() - pattern.length(); ++i)
+    for (size_t i = 0, j; text.length() >= pattern.length() && i <= text.length() - pattern.length(); ++i)
     {
         for (j = 0; j < pattern.length() && text[j + i] == pattern[j]; ++j);
         if (j == pattern.length())
@@ -928,29 +928,36 @@ int strStr(string text, string pattern)
 }
 
 // 29. Divide Two Integers
-int divide(int dividend, int divisor)
+int divide2(int dividend, int divisor)
 {
-    const bool negative = (((dividend ^ divisor) >> 31) & 0x1) == 1;
     unsigned int quotient = 0;
 
-    for (unsigned int a = dividend < 0 ? -dividend : dividend, b = divisor < 0 ? -divisor : divisor; a >= b;)
+    for (unsigned int a = dividend < 0 ? -dividend : dividend, b = divisor < 0 ? -divisor : divisor, k; a >= b; a -= b << k)
     {
-        unsigned int k = 0;
-
         // We are looking for a k that b << k <= a but b << (k + 1) > a. The loop looks complicated since
         // we need to handle overflow. E.g. a == 1 << 31 and b == 1, k should be 31, because b << 31 == a
         // and b << 32 > a, however b << 32 actually overflows so b << 32 == 0, this loop will never end.
         // To prevent overflow, b << k must <= 0x80000000 / 2, otherwise, b << (k + 1) will overflow.
-        for (; a >= b << k && b << k <= (0x80000000 >> 1); ++k);
+        for (k = 0; a >= b << k && b << k <= (0x80000000 >> 1); ++k);
 
         // When loop ends, if a < b << k, it means no overflow happens, and k increased one additional time, we
         // need to decrease k; otherwise (a >= b << k), it means overflow happens, k is the maximum we can find.
         quotient |= 1 << (a < b << k ? --k : k);
-        a -= b << k;
     }
 
     // If negative is false, quotient must <= INT_MAX, otherwise overflow, should return INT_MAX.
-    return negative ? 0 - quotient : quotient > INT_MAX ? INT_MAX : quotient;
+    return (dividend ^ divisor) >> 31 & 0x1 ? 0 - quotient : quotient > INT_MAX ? INT_MAX : quotient;
+}
+int divide(int dividend, int divisor)
+{
+    unsigned int quotient = 0;
+    for (unsigned int a = dividend < 0 ? -dividend : dividend, b = divisor < 0 ? -divisor : divisor, k, temp; a >= b; a -= temp, quotient += k)
+    {
+        // use a-temp >= temp to prevent overflow
+        for (k = 1, temp = b; a - temp >= temp; temp <<= 1, k <<= 1);
+    }
+
+    return (dividend ^ divisor) >> 31 & 1 ? -quotient : quotient > INT_MAX ? INT_MAX : quotient;
 }
 
 // 30. Substring with Concatenation of All Words
